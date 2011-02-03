d997c4cfe34c89cea11a3f1fc44683524c858dd7
Austin Schneider <soccer022483@gmail.com>
2011-02-03 01:49:25 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg', '0.10.0'
+gem 'haml', '3.0.25'
+gem 'compass', '0.10.6'
+gem 'simple_form', '1.3.0'
+gem 'attribute_normalizer', '0.3.1'
+gem 'dalli', '1.0.1'
+
+group :test do
+  gem 'capybara', '~> 0.4.0'
+  gem 'rspec-rails', '~> 2.3.1'
+  gem 'cucumber-rails', '~> 0.3.2'
+  gem 'autotest', '~> 4.4.6'
+  gem 'factory_girl_rails', '~> 1.1.beta1'
+  gem 'database_cleaner', '~> 0.6.0'
+  gem 'launchy', '~> 0.3.7'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgems::Application.config.secret_token = '8681489eaca03fde137bcc631a49800e7a6ccf53a89a343ba0618dfbb993e3365e472b0a01f3537522688c6a0ea28f2aae36304037a945b472ebcb2c034f3f4b'
